46be94f3af7b26bdc6e67af9287132672d85a270
Ashley Likes <alikes1987@gmail.com>
2022-10-27 15:09:43 -0400

    personality quiz day 1

---
+++ View Controllers/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by admin on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - IB Outlets
+    
+    
+    // MARK: - Instance Properties
+    
+    
+    // MARK: - VDL
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+// MARK: - IB Actions
+    
+    
+    
+// MARK: - Instance Methods
+
+}
+
